cbb072d5f74c5e89f5713560947b6d1c57f0198e
Oluwatowo Rosanwo <71358573+CRIMSON-CORP@users.noreply.github.com>
2025-07-30 12:49:03 +0100

    Update webmaster-socket.js

--- webmaster-socket.js
+++ webmaster-socket.js
@@ -2,10 +2,10 @@ import url from "url";
 import { WebSocketServer } from "ws";
 
 const webSocketSecure = new WebSocketServer({
-  port: process.env.PORT || 8080,
+   port: process.env.PORT || 8080
 });
 
-// const BACKEND_URL = "https://fdd.thewatchtower.ae/api/v1";
+const BACKEND_URL = "https://watchtower.thewatchtower.ae/api/v1";
 
 /**
  * @type {Map<string, WebSocket>}
@@ -15,6 +15,7 @@ const clients = new Map();
  * @type {Map<string, WebSocket>}
  */
 const deviceMap = new Map();
+
 /**
  * @type {Map<string, {conversation_user_id:string,conversation_admin_id:string}>}
  */
@@ -33,7 +34,7 @@ webSocketSecure.on("connection", async (webSocket, request) => {
     const json = JSON.parse(Buffer.from(data).toString());
 
     switch (json.type) {
-      case "send_message":
+       case "send_message":
         {
           const payload = {
             ...json,
@@ -108,8 +109,7 @@ webSocketSecure.on("connection", async (webSocket, request) => {
           ];
 
           involvedSockets.forEach((socket) => {
-            socket &&
-              socket.readyState === socket.OPEN &&
+            socket && socket.readyState === socket.OPEN &&
               socket?.send(JSON.stringify(payload));
           });
         }
@@ -136,18 +136,16 @@ webSocketSecure.on("connection", async (webSocket, request) => {
 
           const data = await response.json();
 
-          console.log(data);
+           console.log(data)
 
           const involvedSockets = [
             clients.get(data.data.conversation_user_id.toString()),
             clients.get(data.data.conversation_admin_id.toString()),
           ];
-
+            
           involvedSockets.forEach((socket) => {
-            console.log(socket, "socket");
-            socket &&
-              socket &&
-              socket.readyState === socket.OPEN &&
+             console.log(socket, 'socket')
+            socket && socket && socket.readyState === socket.OPEN &&
               socket?.send(JSON.stringify(payload));
           });
         }
@@ -167,16 +165,14 @@ webSocketSecure.on("connection", async (webSocket, request) => {
           const members = await getConversationMembers(json.conversation_id);
 
           if (!members) return;
-
-          const involvedSockets = [
+           
+         const involvedSockets = [
             clients.get(members.conversation_user_id),
             clients.get(members.conversation_admin_id),
           ];
 
           involvedSockets.forEach((socket) => {
-            socket &&
-              socket &&
-              socket.readyState === socket.OPEN &&
+            socket && socket && socket.readyState === socket.OPEN &&
               socket?.send(JSON.stringify(payload));
           });
         }
@@ -211,8 +207,7 @@ webSocketSecure.on("connection", async (webSocket, request) => {
           ];
 
           involvedSockets.forEach((socket) => {
-            socket &&
-              socket.readyState === socket.OPEN &&
+            socket && socket.readyState === socket.OPEN &&
               socket?.send(JSON.stringify(payload));
           });
         }
@@ -243,8 +238,7 @@ webSocketSecure.on("connection", async (webSocket, request) => {
           ];
 
           involvedSockets.forEach((socket) => {
-            socket &&
-              socket.readyState === socket.OPEN &&
+            socket && socket.readyState === socket.OPEN &&
               socket?.send(JSON.stringify(payload));
           });
         }
@@ -282,230 +276,4 @@ async function getConversationMembers(conversation_id) {
   }
 }
 
-console.log("Webmaster socket server is running");
-
-// import url from "url";
-// import { WebSocketServer } from "ws";
-
-// const webSocketSecure = new WebSocketServer({
-//   port: 8080,
-// });
-
-// const conversations = [
-//   {
-//     id: "123",
-//     members: ["123", "456"],
-//     messages: [
-//       {
-//         id: "",
-//         text: "",
-//         message: "",
-//         sender_id: "",
-//         read: false,
-//       },
-//     ],
-//   },
-//   {
-//     id: "456",
-//     members: ["123", "456"],
-//     messages: [
-//       {
-//         id: "",
-//         text: "",
-//         message: "",
-//         sender_id: "",
-//         read: false,
-//       },
-//     ],
-//   },
-//   {
-//     id: "536345",
-//     members: ["123", "456"],
-//     messages: [
-//       {
-//         id: "",
-//         text: "",
-//         message: "",
-//         sender_id: "",
-//         read: false,
-//       },
-//     ],
-//   },
-// ];
-
-// const clients = new Map();
-
-// webSocketSecure.on("connection", (webSocket, request) => {
-//   const userId = url.parse(request.url, true).query.userId;
-//   console.log(`someone connected! ${userId}`);
-//   clients.set(userId, webSocket);
-
-//   webSocket.on("message", (data) => {
-//     const json = JSON.parse(Buffer.from(data).toString());
-
-//     const conversation = conversations.find(
-//       (conversation) => conversation.id === json.conversation_id
-//     );
-
-//     if (!conversation) return;
-
-//     switch (json.type) {
-//       case "send_message":
-//         conversation?.members?.forEach((member) => {
-//           const ws = clients.get(member);
-//           ws?.send(
-//             JSON.stringify({
-//               ...json,
-//               type: "message_sent",
-//               message: json.message,
-//               read: false,
-//               message_id: json.message_id,
-//               conversation_id: json.conversation_id,
-//               sender_id: json.sender_id,
-//               time: json.time,
-//               ...(json.reply && { reply: json.reply }),
-//             })
-//           );
-//         });
-//         break;
-//       case "delivered_message":
-//         conversation.members.forEach((member) => {
-//           const ws = clients.get(member);
-//           setTimeout(() => {
-//             ws?.send(
-//               JSON.stringify({
-//                 ...json,
-//                 type: "message_delivered",
-//                 message: json.message,
-//                 read: false,
-//                 message_id: json.message_id,
-//                 conversation_id: json.conversation_id,
-//                 sender_id: json.sender_id,
-//                 time: json.time,
-//                 ...(json.reply && { reply: json.reply }),
-//               })
-//             );
-//           }, 500);
-//         });
-//         break;
-//       case "read_message":
-//         conversation.members.forEach((member) => {
-//           const ws = clients.get(member);
-//           setTimeout(() => {
-//             ws?.send(
-//               JSON.stringify({
-//                 type: "message_read",
-//                 message: json.message,
-//                 read: false,
-//                 message_id: json.message_id,
-//                 conversation_id: json.conversation_id,
-//                 sender_id: json.sender_id,
-//                 time: json.time,
-//                 ...(json.reply && { reply: json.reply }),
-//               })
-//             );
-//           }, 500);
-//         });
-//         break;
-//       case "read_conversation":
-//         conversation?.members?.forEach((member) => {
-//           const ws = clients.get(member);
-//           if (ws && ws.readyState === WebSocket.OPEN) {
-//             ws.send(
-//               JSON.stringify({
-//                 type: "conversatoin_read",
-//                 conversation_id: json.conversation_id,
-//                 sender_id: json.sender_id,
-//               })
-//             );
-//           }
-//         });
-//         break;
-//       case "focus":
-//         conversation.members.forEach((member) => {
-//           const ws = clients.get(member);
-
-//           ws?.send(
-//             JSON.stringify({
-//               type: "focus",
-//               conversation_id: json.conversation_id,
-//               sender_id: json.sender_id,
-//             })
-//           );
-//         });
-//         break;
-//       case "active-typing":
-//         conversation.members.forEach((member) => {
-//           const ws = clients.get(member);
-//           ws?.send(
-//             JSON.stringify({
-//               type: "active-typing",
-//               conversation_id: json.conversation_id,
-//               sender_id: json.sender_id,
-//             })
-//           );
-//         });
-//         break;
-//       case "idle-typing":
-//         conversation.members.forEach((member) => {
-//           const ws = clients.get(member);
-//           ws?.send(
-//             JSON.stringify({
-//               type: "idle-typing",
-//               conversation_id: json.conversation_id,
-//               sender_id: json.sender_id,
-//             })
-//           );
-//         });
-//         break;
-//       case "blur":
-//         conversation.members.forEach((member) => {
-//           const ws = clients.get(member);
-//           ws?.send(
-//             JSON.stringify({
-//               type: "blur",
-//               conversation_id: json.conversation_id,
-//               sender_id: json.sender_id,
-//             })
-//           );
-//         });
-//         break;
-//       case "edit_message":
-//         conversation.members.forEach((member) => {
-//           const ws = clients.get(member);
-//           setTimeout(() => {
-//             ws?.send(
-//               JSON.stringify({
-//                 type: "edit_message",
-//                 message: json.message,
-//                 read: false,
-//                 message_id: json.message_id,
-//                 conversation_id: json.conversation_id,
-//                 sender_id: json.sender_id,
-//               })
-//             );
-//           }, 1000);
-//         });
-//         break;
-//       case "delete_message":
-//         conversation?.members?.forEach((member) => {
-//           const ws = clients.get(member);
-//           setTimeout(() => {
-//             ws?.send(
-//               JSON.stringify({
-//                 type: "delete_message",
-//                 message: json.message,
-//                 read: false,
-//                 message_id: json.message_id,
-//                 conversation_id: json.conversation_id,
-//                 sender_id: json.sender_id,
-//               })
-//             );
-//           }, 1000);
-//         });
-//         break;
-//       default:
-//         break;
-//     }
-//   });
-// });
+console.log('Webmaster socket server is running')
